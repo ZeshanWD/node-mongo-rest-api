3e3f31e3eafc8226f6c1971d1dcfe38b1896c1ae
ZeshanWD <zeeshan9choudhary@gmail.com>
2021-06-13 11:47:46 +0200

    confirmAccount and login finished

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 const bcrypt = require('bcrypt');
 const { isValidEmail } = require('../helpers');
@@ -30,8 +31,10 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+UserSchema.statics.confirmAccount = confirmAccount;
 UserSchema.statics.signup = signup;
 UserSchema.statics.sendConfirmationEmail = sendConfirmationEmail;
+UserSchema.statics.login = login;
 
 mongoose.model('user', UserSchema, 'users');
 
@@ -39,7 +42,7 @@ mongoose.model('user', UserSchema, 'users');
 // static methods
 function signup(userInfo) {
   if (!userInfo.email || !isValidEmail(userInfo.email)) throw new Error('email is invalid');
-  if (!userInfo.password) throw new Error(new Error('password is required'));
+  if (!userInfo.password) throw new Error('password is required');
   if (!userInfo.firstName) throw new Error('firstName is required');
   if (!userInfo.lastName) throw new Error('lastName is required');
 
@@ -57,7 +60,7 @@ function signup(userInfo) {
       return this.create(newUser);
     })
     .then(userCreated => this.sendConfirmationEmail(userCreated))
-    .then(user => user)
+    .then(user => user);
 }
 
 function sendConfirmationEmail(user) {
@@ -71,10 +74,60 @@ function sendConfirmationEmail(user) {
     },
   });
 
+  var token = jwt.sign({ email: user.email }, process.env.TOKEN_SECRET);
+
+  const urlConfirm = `${process.env.APIGATEWAY_URL}/account/confirm/${token}`;
   return transporter.sendMail({
     from: process.env.MAIL_ADMIN_ADDRESS,
     to: user.email,
     subject: "Please confirm your email",
-    html: "<b>Confirm Your Email!</b>",
+    html: `<p>Confirm Your Email: <a href="${urlConfirm}">Confirm</a></p>`,
   }).then(() => user)
+}
+
+function confirmAccount(token) {
+  var email = null;
+  try {
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+    email = payload.email;
+  } catch(err) {
+    throw new Error('invalid token');
+  }
+
+  return this.findOne({ email })
+    .then(user => {
+      if (!user) throw new Error('user not found');
+      if (user.emailVerified) throw new Error('user already verified');
+
+      user.emailVerified = true;
+      return user.save();
+    });
+}
+
+function login(email, password) {
+  if (!isValidEmail(email)) throw new Error('email is invalid');
+
+  return this.findOne({ email })
+    .then(user => {
+      if (!user) throw new Error('incorrect credentials');
+      if (!user.emailVerified) throw new Error('user is not verified');
+
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (!isMatch) throw new Error('incorrect credentials');
+
+      const userObject = {
+        _id: user._id,
+        email: user.email,
+        emailVerified: user.emailVerified,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      };
+      const access_token = jwt.sign(Object.assign({}, userObject), process.env.TOKEN_SECRET, {
+        expiresIn: 60 * 60 * 4, // seconds
+      })
+
+      return {
+        access_token,
+      }
+    })
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -12,9 +12,39 @@ router.post('/signup', (req, res) => {
     User.signup(req.body.user)
       .then(() => {
         res.status(200).send({ success: true, message: 'user created successfully' });
-      }).catch(error => res.status(200).send({ success: false, error: error.message }))
+      }).catch(error => res.status(200).send({ success: false, error: error.message }));
   } catch(error) {
-    res.status(200).send({ success: false, error: error.message })
+    res.status(500).send({ success: false, error: error.message })
+  }
+});
+
+
+router.get('/confirm/:token', (req, res) => {
+  const User = getModelByName('user');
+
+  try {
+    User.confirmAccount(req.params.token)
+      .then(() => {
+        res.status(200).send({ success: true, message: "user confirmed succesfully" });
+      }).catch(err => res.status(200).send({ success: false, error: err.message }))
+  } catch(err) {
+    res.status(200).send({ success: false, error: err.message });
+  }
+});
+
+router.post('/login', (req, res) => {
+  if (!req.body.email) return res.status(200).send({ success: false, error: "email not provided" });
+  if (!req.body.password) return res.status(200).send({ success: false, error: "password not provided" });
+
+  const User = getModelByName('user');
+
+  try {
+    User.login(req.body.email, req.body.password)
+      .then((data) => {
+        res.status(200).send({ success: true, data });
+      }).catch(err => res.status(200).send({ success: false, error: err.message }))
+  } catch(err) {
+    res.status(200).send({ success: false, error: err.message });
   }
 });
 
